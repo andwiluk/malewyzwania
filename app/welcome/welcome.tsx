cdf44739cc2bc545d39241e0fcc851f36fc2fa22
Andrzej <andwiluk@gmail.com>
2025-05-06 20:02:35 +0200

    Add more maps

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -4,6 +4,11 @@ import footer from "./img/footer.png";
 import map1 from "./img/mapa1.png";
 import map2 from "./img/mapa2.png";
 import map3 from "./img/mapa3.png";
+import map4 from "./img/mapa4.png";
+import map5 from "./img/mapa5.png";
+import map6 from "./img/mapa6.png";
+import map7 from "./img/mapa7.png";
+import map8 from "./img/mapa8.png";
 import wyzwaniaTop from "./img/wyzwaniaTop.png";
 
 export function Welcome() {
@@ -16,7 +21,7 @@ export function Welcome() {
     }
   }, []);
 
-  const maps: Record<number, string> = { 1: map1, 2: map2, 3: map3 };
+  const maps: Record<number, string> = { 1: map1, 2: map2, 3: map3, 4: map4, 5: map5, 6: map6, 7: map7, 8: map8 };
 
   return (
     <main className="flex items-center justify-center pb-4 w-[1650px] mx-auto">
@@ -35,11 +40,16 @@ export function Welcome() {
           transition: "background-image 0.8s ease-in-out",
         }}
       >
-        <Button positionX={1370} positionY={2090} mapId={2} setMapId={setMapId} />
-
-        <Button positionX={560} positionY={1790} mapId={3} setMapId={setMapId} />
-
         <Button positionX={343} positionY={2120} mapId={1} setMapId={setMapId} />
+        <Button positionX={1370} positionY={2090} mapId={2} setMapId={setMapId} />
+        <Button positionX={560} positionY={1790} mapId={3} setMapId={setMapId} />
+        <Button positionX={175} positionY={1415} mapId={4} setMapId={setMapId} />
+        <Button positionX={895} positionY={1275} mapId={5} setMapId={setMapId} />
+        <Button positionX={165} positionY={810} mapId={6} setMapId={setMapId} />
+        <Button positionX={1075} positionY={470} mapId={7} setMapId={setMapId} />
+        <Button positionX={690} positionY={150} mapId={8} setMapId={setMapId} />
+
+        
       </div>
 
       <div className="max-w-[1650px] w-full space-y-0 px-4 footer">
