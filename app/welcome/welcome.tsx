0607160036435107c99478aa209a1c332bed2802
Andrzej Wiluk <andrzej.wiluk@equiqo.com>
2025-06-05 18:06:52 +0200

    Add images, start dynamic map

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -11,6 +11,7 @@ import map7 from "./img/mapa7.png";
 import map8 from "./img/mapa8.png";
 import wyzwaniaTop from "./img/wyzwaniaTop.png";
 import TripModal from "~/components/TripModal/TripModal";
+import { Route } from "react-router";
 
 export function Welcome() {
   const [mapId, setMapId] = useState<number>(parseInt("1"));
@@ -41,63 +42,72 @@ export function Welcome() {
         <div className="max-w-[1650px] w-full space-y-0 px-4 header"></div>
         {/* </header> */}
         <div className="max-w-[1650px] max-w-[1650px] w-full space-y-0 px-4 challengesTop"></div>
-        <div
+        {/* <div
           className="max-w-[1650px] w-full space-y-0 px-4 map"
           style={{
             backgroundImage: `url(${maps[mapId]})`,
             transition: "background-image 0.8s ease-in-out",
           }}
+        > */}
+        <div
+          className="max-w-[1650px] w-full space-y-0 px-4 map"
+          style={{
+            backgroundImage: `url(assets/mapa-0.jpg)`,
+            transition: "background-image 0.8s ease-in-out",
+          }}
         >
-          <Button
-            positionX={343}
-            positionY={2120}
-            mapId={1}
-            setMapId={setMapId}
-          />
-          <Button
-            positionX={1370}
-            positionY={2090}
-            mapId={2}
-            setMapId={setMapId}
-          />
-          <Button
-            positionX={560}
-            positionY={1790}
-            mapId={3}
-            setMapId={setMapId}
-          />
-          <Button
-            positionX={175}
-            positionY={1415}
-            mapId={4}
-            setMapId={setMapId}
-          />
-          <Button
-            positionX={895}
-            positionY={1275}
-            mapId={5}
-            setMapId={setMapId}
-          />
-          <Button
-            positionX={165}
-            positionY={810}
-            mapId={6}
-            setMapId={setMapId}
-          />
-          <Button
-            positionX={1075}
-            positionY={470}
-            mapId={7}
-            setMapId={setMapId}
-          />
-          <Button
-            positionX={690}
-            positionY={150}
-            mapId={8}
-            setMapId={setMapId}
-          />
+          <div>
+            {/* <Button
+              positionX={343}
+              positionY={2120}
+              mapId={1}
+              setMapId={setMapId}
+            /> */}
+            <Button
+              positionX={1200}
+              positionY={2030}
+              mapId={1}
+              setMapId={setMapId}
+            />
+            {/* <Button
+              positionX={560}
+              positionY={1790}
+              mapId={3}
+              setMapId={setMapId}
+            /> */}
+            {/*}
+            <Button
+              positionX={175}
+              positionY={1415}
+              mapId={4}
+              setMapId={setMapId}
+            />
+            <Button
+              positionX={895}
+              positionY={1275}
+              mapId={5}
+              setMapId={setMapId}
+            />
+            <Button
+              positionX={165}
+              positionY={810}
+              mapId={6}
+              setMapId={setMapId}
+            />
+            <Button
+              positionX={1075}
+              positionY={470}
+              mapId={7}
+              setMapId={setMapId}
+            />
+            <Button
+              positionX={690}
+              positionY={150}
+              mapId={8}
+              setMapId={setMapId}
+            /> */}
+          </div>
         </div>
-
         <div className="max-w-[1650px] w-full space-y-0 px-4 footer"></div>
       </div>
 
@@ -174,26 +184,45 @@ export function Button({
   };
 
   return (
-    <div className={className} onClick={handleClick}>
+    <>
+      <button className={className} onClick={handleClick}></button>
+      <div className={`route route${mapId}`}></div>
       <style>
         {`
         .xButton${mapId} {
-          height: 200px;
-          width: 200px;
+          height: 285px;
+          width: 300px;
           background-color: transparent;
           cursor: pointer;
           position: absolute;
           top: ${positionY}px;
           left: ${positionX}px;
           z-index: 10;
+          background-image: url(/assets/Krok${mapId}/start1.png);
 
           // transition: background-color 0.3s ease;
-          // &:hover {
-          //   background-color: rgba(255, 255, 255, 0.5);
-          // }
+          &:hover {
+            background-image: url(/assets/Krok${mapId}/start2.png);
+          }
+        }
+
+        .route${mapId} {
+          position: absolute;
+          background-image: url(/assets/Krok${mapId}/path_gray.png);
+          // top: ${positionY + 100}px;
+          // left: ${positionX + 100}px;
+          top: 1810px;
+          left: 415px;
+          width: 895px;
+          height: 489px;
+          display: none;
+        }
+
+        .xButton${mapId}:hover + .route${mapId} {
+          display: block;
         }
       `}
       </style>
-    </div>
+    </>
   );
 }
